08922240b0548d3264d5ecafae741ad0aa85d661
Eugene19892 <81512272+Eugene19892@users.noreply.github.com>
2021-04-01 13:51:34 +0300

    Update HW2.java

--- src/lesson02/HW2.java
+++ src/lesson02/HW2.java
@@ -2,21 +2,19 @@ package lesson02;
 
 //public class HW2 {
 //метод1
-//    public static void main(String[] args) {
-//
-//        int[] array = new int[1, 1, 0, 0, 1, 0, 1, 1, 0, 0];
-//        int value = 1;
-//        for (int i = 1; i < array.length; i--)
-//            int value = 0;
-//        for (int i = 0; i < array.length; i++)
-//            while (i < 10) {
-//
-//
-//                System.out.println(array[i]);
-//            }
-//
-//    }
-//}
+//    static void changeArray() {
+        int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+     
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            if (arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+        
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
    /*
    ..метод 2
     public static void main(String[] args) {
